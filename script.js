6c7c295162eb6c5c59a1d78e0d21a66d4f48ed78
NamanMathur <36097162+NamanMathur77@users.noreply.github.com>
2018-08-19 23:05:38 +0530

    A todo application to remind you, your tasks for today

---
+++ script.js
@@ -0,0 +1,65 @@
+
+var ul=document.getElementById('list');       //initialising unordered list
+var li;
+
+
+
+
+
+
+// alert("Connected")
+var addButton=document.getElementById('add');
+addButton.addEventListener('click', addItem);     //addItem is a function defined later
+
+var removeButton=document.getElementById('remove');
+removeButton.addEventListener('click', removeItem);       //removeItem is a function defined later
+
+
+function removeItem(){
+    //li contains all the children of ul
+    li=ul.children;
+    for(let index=0; index<li.length; index++){
+        //li is an array which contains all the elements
+        //each element in li have children which is either checked or unchecked
+        while(li[index] && li[index].children[0].checked){
+            //if element is checked it will be removed
+            ul.removeChild(li[index])
+        }
+    }
+}
+
+function addItem(){
+    var input=document.getElementById('input');//initialising text input 
+    var item=input.value;
+    ul=document.getElementById('list');//ul is the list of todos
+    var textnode=document.createTextNode(item);//making a text node from the input from the user
+    
+    //if user inputs empty string
+    if(item==''){
+        return false;
+    }
+
+
+    else{
+
+        //initialising the elements to create
+        li=document.createElement('li');
+        var checkbox=document.createElement('input')
+        checkbox.type='checkbox';
+        checkbox.setAttribute('id','check');
+        var label=document.createElement('label');
+        label.setAttribute('for','item');
+
+        //adding the elements in the format ul->li->checkbox,label->textnode
+        ul.appendChild(label);
+        li.appendChild(checkbox);
+        label.appendChild(textnode);
+        li.appendChild(label);
+        ul.insertBefore(li, ul.childNodes[0]);
+        //to create a fade in effect
+        setTimeout(()=>{
+            li.className='visual';
+        },100);
+        input.value='';
+    }
+}
